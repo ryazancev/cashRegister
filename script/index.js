02bd7acc8806f8a6f7bfdd8b2e15659a864a3439
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2021-03-14 18:51:28 +0300

    initial commit

---
+++ script/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import slider from "./module/slider.js";
+
+slider();
---
+++ script/module/slider.js
@@ -0,0 +1,38 @@
+const slider = () => {
+    const
+        slide = document.querySelectorAll('.slide'),
+        sliderContainer = document.querySelector('.main-slider');
+
+    let currentSlide = 0, //Текущий слайд
+        interval; //Интервал для setInterval
+
+    const prevSlide = (elem, index, strClass) => {
+        elem[index].classList.remove(strClass);
+    };
+
+    const nextSlide = (elem, index, strClass) => {
+        elem[index].classList.add(strClass);
+    };
+
+    const autoPlaySlide = () => {
+        prevSlide(slide, currentSlide, 'slide--active');
+
+        currentSlide++;
+
+        if (currentSlide >= slide.length) currentSlide = 0;
+
+        nextSlide(slide, currentSlide, 'slide--active');
+    };
+
+    const startSlide = (time = 2500) => {
+        interval = setInterval(autoPlaySlide, time);
+    };
+
+    const stopSlide = () => {
+        clearInterval(interval);
+    };
+
+    startSlide();
+}
+
+export default slider;
